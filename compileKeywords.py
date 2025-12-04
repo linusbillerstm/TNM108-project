99e816366e073775207267eb4d4a0b79765245c4
billerstam <119046929+linusbillerstm@users.noreply.github.com>
2025-12-04 10:17:21 +0100

    uppdaterat

--- compileKeywords.py
+++ compileKeywords.py
@@ -1,18 +1,8 @@
 import pandas as pd
 
-# läs data
-df = pd.read_csv("imdb_tvshows.csv")
+df = pd.read_csv("shows.csv")
 
-features = ['About', 'Genres', 'Actors']
-for feature in features:
-    df[feature] = df[feature].fillna('')
+features = ['rank', 'title', 'year', 'imdb_votes', 'imdb_rating', 'duration', 'genre', 'cast_name', 'director_name', 'storyline']
+df_sorted = df[features]
 
-def combine_features(row):
-    return f"{row['About']} {row['Genres']} {row['Actors']}"
-
-# Applicera funktionen
-df['combined_features'] = df.apply(combine_features, axis=1)
-
-# Spara den nya filen som du sedan laddar i din huvudapp
-df.to_csv("processed_tvshows.csv", index=False)
-print("Data förbehandlad och sparad!")
+df_sorted.to_csv('sorted_shows.csv')
