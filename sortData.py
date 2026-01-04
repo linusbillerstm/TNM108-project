aebad52883edf2aadf62ffeb0dd042bd4063161e
billerstam <119046929+linusbillerstm@users.noreply.github.com>
2026-01-04 14:32:01 +0100

    Update sorting_data.py

--- sortData.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("shows.csv")
-
-features = ['rank', 'title', 'year', 'imdb_votes', 'imdb_rating', 'duration', 'genre', 'cast_name', 'director_name', 'storyline']
-df_sorted = df[features]
-
-df_sorted.to_csv('sorted_shows.csv')
